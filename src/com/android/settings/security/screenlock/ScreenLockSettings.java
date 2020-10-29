3d2eea9ade78a2cb0b62c1a33b63aeab6e673705
daniml3 <danimoral1001@gmail.com>
2020-10-29 16:40:52 +0100

    Hide FOD Animations preferences if device does not support it
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Change-Id: I4be4657755893970a2e4755bfd0a3dd5453f3249

--- src/com/android/settings/security/screenlock/ScreenLockSettings.java
+++ src/com/android/settings/security/screenlock/ScreenLockSettings.java
@@ -80,6 +80,8 @@ public class ScreenLockSettings extends DashboardFragment
                 context, MY_USER_ID, lockPatternUtils));
         controllers.add(new QuickUnlockPreferenceController(
                 context, MY_USER_ID, lockPatternUtils));
+        controllers.add(new FODAnimationsSwitchPreferenceController(context));
+        controllers.add(new FODAnimationsListPreferenceController(context));
         return controllers;
     }
 
